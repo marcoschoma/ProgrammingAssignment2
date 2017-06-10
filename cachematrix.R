f14fae9387bd4acff31d42653d2da8ebd3400560
marcos braga choma <marcos.choma@gmail.com>
2017-06-10 06:45:02 -0300

    First submission - pa2 lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#Creating the cache
+## Starting from a new matrix or having it passed to the function, 'makeCacheMatrix' will return a list with four functions.
+## This four functions enables the cache maintenance. Two of them are meant to be used internally, and two externally.
+## The external functions, setsolve and getsolve will respectivelly set and get the data to be managed
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(val) m <<- val
+  getsolve <- function() m
+  list(set = set, get = get,
+    setsolve = setsolve,
+    getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+# Using cache to increase performance
+## Given an previously created cache (see makeCacheMatrix above), cacheSolve will return an inverted given matrix.
+## Only the last cached object will be returned. If the matrix changes, makeCacheMatrix must be called again
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  m <- solve(...)
+  x$setsolve(m)
+  m
 }
